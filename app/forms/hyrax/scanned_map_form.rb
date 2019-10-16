168c30f595f22c5ecd0ca80fe6de57cff0a4ebdc
GriffinJ <jrgriffiniii@gmail.com>
2019-10-16 12:56:53 -0400

    Fixing the support for single/multiple-valued fields

--- app/forms/hyrax/scanned_map_form.rb
+++ app/forms/hyrax/scanned_map_form.rb
@@ -7,15 +7,30 @@ module Hyrax
     self.terms += [:resource_type, :coverage]
     self.required_fields += [:coverage]
 
+    def self.spatial_fields
+      [:coverage]
+    end
+
+    def self.single_valued_fields
+      [:coverage]
+    end
+
+    def self.multiple?(field)
+      if single_valued_fields.include?(field.to_sym)
+        false
+      else
+        super
+      end
+    end
+
     # This provides the casting for RDF literals
     # Until these are natively handled by ActiveTriples, this will be necessary
     def self.model_attributes(_form_params)
       super.tap do |processed_params|
-        terms.each do |key|
-          if key == :coverage
-            # This should also check for multiple?
-            existing_values = processed_params[key] || []
-            processed_params[key] = existing_values.map { |spatial| WellKnownTextLiteral.new(spatial) }
+        terms.each do |term|
+          if spatial_fields.include?(term)
+            existing_values = processed_params[term] || []
+            processed_params[term] = existing_values.map { |spatial| WellKnownTextLiteral.new(spatial) }
           end
         end
       end
