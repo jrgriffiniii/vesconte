200a1116da3377b40e09b3fd34a0e407912197ae
GriffinJ <jrgriffiniii@gmail.com>
2019-10-21 20:59:09 -0500

    Ensuring that the application is renamed

--- config/application.rb
+++ config/application.rb
@@ -7,7 +7,7 @@ require 'active_job/arguments'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module MyHyrax
+module Vesconte
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
@@ -18,6 +18,7 @@ module MyHyrax
 
     # This enables RDF::Literal attribute values to be offered in arguments for
     # the ActiveJob
+    # Please see https://github.com/rails/rails/blob/5-1-stable/activejob/lib/active_job/arguments.rb#L25
     ::ActiveJob::Arguments::TYPE_WHITELIST.push(RDF::Literal)
   end
 end
